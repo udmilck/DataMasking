727e0943ed169c6683729a7a93f207700f3112b6
RoyLin <ecustlin@iCloud.com>
2020-06-22 15:33:44 +0800

    Update main.py
    add four types datamasking

--- main.py
+++ main.py
@@ -13,19 +13,30 @@ def getStrAsMD5(parmStr):
     m.update(parmStr)
     return m.hexdigest()
 
-
-# print("值:\n{0}".format(getStrAsMD5('参字第1234567号')))
 df=pd.read_excel('test.xlsx')
 ids=df['身份证件'].values
 names=df['户名'].values
-print(df['身份证件'].str)
+
 # 身份证号, 1/2代处理
-RegExp='(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0[1-9]|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)'
-ids= df['身份证件'].replace(RegExp,df['身份证件'], regex=True)
-# 警官证处理
-# ids= df['身份证件'].replace('^[\u4E00-\u9FA5]{2}$','test', regex=True)
+IdReg='(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0[1-9]|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)'
+ids= df['身份证件'].replace(IdReg,df['身份证件'].apply(lambda x:x[0:6]+getStrAsMD5(x)), regex=True)
 print("值:\n{0}".format(ids))
-# print("值:\n{0}".format(names))
+
+#passport 处理
+preg='(^[EeKkGgDdSsPpHh]\d{8}$)|(^(([Ee][a-fA-F])|([DdSsPp][Ee])|([Kk][Jj])|([Mm][Aa])|(1[45]))\d{7}$)'
+ids= df['身份证件'].replace(preg,df['身份证件'].apply(lambda x:x[0:6]+getStrAsMD5(x)), regex=True)
+print("值:\n{0}".format(ids))
+
+# 军官证处理
+jreg = '(^[\u4E00-\u9FA5](字第)([0-9a-zA-Z]{4,8})(号?)$)'
+ids= df['身份证件'].replace(jreg,df['身份证件'].apply(lambda x:x[0:2]+getStrAsMD5(x)), regex=True)
+print("值:\n{0}".format(ids))
+
+#警官证处理
+policereg = '(^[\u4E00-\u9FA5]([0-9a-zA-Z]{7})$)'
+ids= df['身份证件'].replace(policereg,df['身份证件'].apply(lambda x:x[0]+getStrAsMD5(x)), regex=True)
+print("值:\n{0}".format(ids))
+
 #户名处理
-# names= df['户名'].replace('^[\u4E00-\u9FA5]{2,3}$',df['户名'].str[-1], regex=True)
-# print("值:\n{0}".format(names))
+names= df['户名'].replace('^[\u4E00-\u9FA5]{2,3}$',df['户名'].str[-1], regex=True)
+print("值:\n{0}".format(names))
